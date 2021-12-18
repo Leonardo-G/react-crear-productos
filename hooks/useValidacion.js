9a7737bc5c737a4fec347287bf5ca431a50568b7
leonardog9823 <guanuco43@gmail.com>
2021-12-18 01:02:40 -0300

    formulario y sus estilos

---
+++ hooks/useValidacion.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+
+const useValidacion = ( stateInicial, validar, fn ) => {
+
+    const [valores, setValores] = useState(stateInicial);
+    const [errores, setErrores] = useState({});
+    const [submitForm, setSubmitForm] = useState(false);
+
+    useEffect(() => {
+        if(submitForm){
+            const noErrores = Object.keys(errores).length === 0;
+
+            if(noErrores){
+                fn(); //Fn = Funcion que se ejecut en el componente
+            }
+            setSubmitForm(false)
+        }
+    }, [ errores ])
+
+    //Funcion que se ejecuta conforme el usuario escribe algo
+    const handleChange = e => {
+        setValores({
+            ...valores,
+            [ e.target.name ]: e.target.value
+        })
+    }
+
+    //Funcion que se ejecuta cuando el usuario hacec submit
+    const handleSubmit = e => {
+        e.preventDefault();
+        const erroresValidacion = validar(valores);
+        setErrores(erroresValidacion);
+        setSubmitForm(true);
+    }
+
+    //Cuando se realizar el evento
+    const handleBlur = () => {
+        const erroresValidacion = validar(valores);
+        setErrores(erroresValidacion);
+    }
+    return {
+        valores, 
+        errores,
+        handleSubmit, 
+        handleChange,
+        handleBlur
+    }
+}
+
+export default useValidacion
--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,9 +1,91 @@
-import React from 'react'
+import React from 'react';
+import { Layout } from '../components/layout/Layout';
+import { validarCrearCuenta } from '../helpers/validarCuenta';
+import useValidacion from '../hooks/useValidacion';
+
+import styles from "../styles/Formularios.module.css"
+
+const state_Inicial = {
+    nombre: "",
+    email: "",
+    password: ""
+}
 
 const CrearCuenta = () => {
+
+    const crearCuenta = () => {
+        console.log("creando cuenta")
+    }
+
+    const { valores, errores, handleSubmit, handleChange, handleBlur } = useValidacion(state_Inicial, validarCrearCuenta, crearCuenta);
+
+    const { nombre, email, password } = valores
+
+
     return (
         <div>
-            
+            <Layout>
+                <h1 className={ styles.titulo_formulario }>Crear Cuenta</h1>
+                <form 
+                    className={ styles.formulario } 
+                    onSubmit={ handleSubmit }
+                    noValidate
+                >
+                    <div className={ styles.campo }>
+                        <label htmlFor='nombre' className={ styles.campo_label }>Nombre</label>
+                        <input 
+                            className={ styles.campo_input }
+                            type="text"
+                            id="nombre"
+                            placeholder='Tu Nombre'
+                            name='nombre'
+                            value={nombre}
+                            onChange={ handleChange }
+                            onBlur={ handleBlur }
+                        />
+                    </div>
+                    {
+                        errores.nombre && <p className={ styles.errores }>{ errores.nombre }</p>
+                    }
+                    <div className={ styles.campo }>
+                        <label htmlFor='email' className={ styles.campo_label }>Email</label>
+                        <input 
+                            className={ styles.campo_input }
+                            type="email"
+                            id="email"
+                            placeholder='Tu Email'
+                            name='email'
+                            value={ email }
+                            onChange={ handleChange }
+                            onBlur={ handleBlur }
+                        />
+                    </div>
+                    {
+                        errores.email && <p className={ styles.errores }>{ errores.email }</p>
+                    }
+                    <div className={ styles.campo }>
+                        <label htmlFor='password' className={ styles.campo_label }>Password</label>
+                        <input 
+                            className={ styles.campo_input }
+                            type="password"
+                            id="password"
+                            placeholder='Tu Password'
+                            name='password'
+                            value={ password }
+                            onChange={ handleChange }
+                            onBlur={ handleBlur }
+                        />
+                    </div>
+                    {
+                        errores.password && <p className={ styles.errores }>{ errores.password }</p>
+                    }
+                    <input 
+                        className='boton boton--orange block'
+                        type="submit"
+                        value="Crear Cuenta"
+                    />
+                </form>
+            </Layout>
         </div>
     )
 }
